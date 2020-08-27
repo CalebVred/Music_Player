53cf03810718c03f56098e9ea1575a92853201ad
Caleb Vredevoogd <cvredevoogd99@gmail.com>
2020-08-26 21:44:46 -0400

    Improves documentation and readability

--- Podcast_DLoader.py
+++ Podcast_DLoader.py
@@ -6,17 +6,6 @@ from pytube import YouTube
 import os
 import platform
 
-'''
-from selenium import webdriver
-from selenium.webdriver import Chrome
-#from selenium.webdriver import Firefox
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-#from selenium.webdriver.firefox.options import Options
-#from selenium.webdriver import ActionChainspytube
-'''
-
-
 #Track_dLoader class takes in a source (RSS) feed and finds
 #and downloads the latest episode based on the show title
 class Track_dLoader:
@@ -42,63 +31,55 @@ class Track_dLoader:
         self.dLoadEList()  
         
     
-    #input: url(str), a link to a YouTube channel in HTML or RSS xml form
-    #
+    #Input: url(str), a link to a YouTube channel in HTML or RSS xml form
+    #Output: rssURL, a proper RSS-friendly URL
     #Converts html address of YouTube channel to RSS address
     def handleYTLink(self, url):
         
         #If HTML prefix is present, search for it and replace it with the RSS prefix
         rss = re.compile(r'https://www.youtube.com/channel/')
-        workingURL = rss.sub(r'https://www.youtube.com/feeds/videos.xml?channel_id=', url)
+        rssURL = rss.sub(r'https://www.youtube.com/feeds/videos.xml?channel_id=', url)
         
-        return workingURL
-
+        return rssURL
+    
+    #Input: None
+    #Output: None
     #Retrieves a list of podcast episodes from RSS feed
     def dLoadEList(self):
         feed = feedparser.parse(self.feedURL)
         
         #Appends to self.episodeList RSS entries that contain the show title
         for entry in feed.entries:
-            if(self.containsTitle(entry.title, self.showTitle)) != -1:
+            if(self.containsTitle(entry.title, self.showTitle)) == True:
                 self.episodeList.append(entry)
 
-    #Retrieve local episode list
+    #Input: None
+    #Output: self.episodeList (List)
+    #Retrieves local episode list
     def getEList(self):
         return self.episodeList
 
+    #Input: None
+    #Output: True if the title is present or the episode number if available
+    #Returns False if the title is not present
     #Searches the string RSS item title itemT for substring show title showT
-    #Returns 0 if the title is present or the episode number if available
-    #Returns -1 if the title is not present
     def containsTitle(self, entryT, showT):
         search = entryT.find(showT)
         if search != -1:
-            return 0
+            return True
         else:
-            return -1
+            return False
     
-    #Download YouTube video an RSS feed item that links to a YouTube video
+    #Input: episode
+    #Output: None
+    #Downloads YouTube video to destination directory 
     def dLoadYT(self, episode):
+        #Download YouTube video or throw error
         try:
-            
             YouTube(episode.link).streams.first().download(self.dest)
         except:
             print("Connection Error: link might be invalid or video could be removed")
 
-    
-            
-    '''
-    #Open the link with Selenium
-    def openAndDownload(self, episode):
-        #TODO: May need to make separate API for handling Zencast modules
-        
-        actions = ActionChains(self.driver)
-        #Follow the episode link
-        self.driver.get(episode.link)
-        shareButtonXPath = "/html/body/div[2]/div/div/div[3]/div/div[1]/button[3]"
-        shareButton = self.driver.find_el'Flight Sim 2020 will CRUSH Your Gaming Rig- WAN Show August 21 2020.mp4'ement_by_xpath(shareButtonXPath)
-        actions.click(shareButton)
 
-        dLoadButtonXPath = "/html/body/div[2]/div/div/div[3]/div[1]/div[2]/h6"
-        dLoadButton = self.driver.find_elements_by_xpath(dLoadButtonXPath)
-        actions.context_click(dLoadButton).perform()
-    '''
+
+    
