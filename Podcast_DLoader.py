7795291501e8a876fe1ab8f4b0453047495c7d80
Caleb Vredevoogd <cvredevoogd99@gmail.com>
2020-08-26 21:28:17 -0400

    Adds handling of normal YT channel URLs

--- Podcast_DLoader.py
+++ Podcast_DLoader.py
@@ -5,14 +5,15 @@ from pytube import YouTube
 import os
 import platform
 
+'''
 from selenium import webdriver
 from selenium.webdriver import Chrome
 #from selenium.webdriver import Firefox
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 #from selenium.webdriver.firefox.options import Options
-#from selenium.webdriver import ActionChains
-
+#from selenium.webdriver import ActionChainspytube
+'''
 
 
 #Track_dLoader class takes in a source (RSS) feed and finds
@@ -21,16 +22,35 @@ class Track_dLoader:
     #Initialize
     def __init__(self, feedURL, showTitle, dest):
         #feedparser
-        self.episodeList = []
+        self.episodeList =AN Sh []
         self.feedURL = feedURL
         self.showTitle = showTitle
         self.dest = dest
        
-        opt = webdriver.ChromeOptions()
-        opt.binary_location = "~/chromedriver"
+        #opt = webdriver.ChromeOptions()
+        #opt.binary_location = "~/chromedriver"
         #Selenium
         #self.driver = webdriver.Chrome(chrome_options=opt, executable_path='~/chromedriver')
         #TODO: This triggers an error
+        
+        #Handle YouTube links
+        if (re.search(r'youtube.com', self.feedURL)):
+            self.feedURL = self.handleYTLink(self.feedURL)
+
+        #Retrieve podcast episode list
+        self.dLoadEList()  
+        
+    
+    #input: url(str), a link to a YouTube channel in HTML or RSS xml form
+    #
+    #Converts html address of YouTube channel to RSS address
+    def handleYTLink(self, url):
+        
+        #If HTML prefix is present, search for it and replace it with the RSS prefix
+        rss = re.compile(r'https://www.youtube.com/channel/')
+        workingURL = rss.sub(r'https://www.youtube.com/feeds/videos.xml?channel_id=', url)
+        
+        return workingURL
 
     #Retrieves a list of podcast episodes from RSS feed
     def dLoadEList(self):
@@ -41,12 +61,10 @@ class Track_dLoader:
             if(self.containsTitle(entry.title, self.showTitle)) != -1:
                 self.episodeList.append(entry)
 
-    
     #Retrieve local episode list
     def getEList(self):
         return self.episodeList
 
-                       
     #Searches the string RSS item title itemT for substring show title showT
     #Returns 0 if the title is present or the episode number if available
     #Returns -1 if the title is not present
--- dLoader_tester.py
+++ dLoader_tester.py
@@ -1,15 +1,27 @@
 from Podcast_DLoader import Track_dLoader
 
 
-#Downloads the latest episode of the WAN show
-print("dLoader2: Testing 'https://www.youtube.com/feeds/videos.xml?channel_id=UCXuqSBlHAE6Xw-yeJA0Tunw'")
-dLoader2 = Track_dLoader("https://www.youtube.com/feeds/videos.xml?channel_id=UCXuqSBlHAE6Xw-yeJA0Tunw",
+#Downloads the latest episode of the WAN show using RSS URL
+print("dLoader1a: Testing 'https://www.youtube.com/feeds/videos.xml?channel_id=UCXuqSBlHAE6Xw-yeJA0Tunw'")
+dLoader1a = Track_dLoader("https://www.youtube.com/feeds/videos.xml?channel_id=UCXuqSBlHAE6Xw-yeJA0Tunw",
                          "WAN Show", "Downloads")
-dLoader2.dLoadEList()
-assert(len(dLoader2.getEList()) > 0)
-print("dLoader2 Episode List not empty")
-latest_ep = dLoader2.getEList()[0]
+assert(len(dLoader1a.getEList()) > 0)
+print("dLoader1a Episode List not empty")
+latest_ep = dLoader1a.getEList()[0]
 print("Latest episode title:", latest_ep.title)
 #print("Latest episode description:", latest_ep.description)
 print("Latest episode link:", latest_ep.link)
-dLoader2.dLoadYT(latest_ep)
+dLoader1a.dLoadYT(latest_ep)
+
+#Downloads the latest episode of the WAN show using html URL
+#Downloads the latest episode of the WAN show using RSS URL
+print("dLoader1b: Testing 'https://www.youtube.com/channel/UCXuqSBlHAE6Xw-yeJA0Tunw'")
+dLoader1b = Track_dLoader("https://www.youtube.com/channel/UCXuqSBlHAE6Xw-yeJA0Tunw",
+                         "WAN Show", "Downloads")
+assert(len(dLoader1b.getEList()) > 0)
+print("dLoader1b Episode List not empty")
+latest_ep = dLoader1b.getEList()[0]
+print("Latest episode title:", latest_ep.title)
+#print("Latest episode description:", latest_ep.description)
+print("Latest episode link:", latest_ep.link)
+dLoader1b.dLoadYT(latest_ep)
